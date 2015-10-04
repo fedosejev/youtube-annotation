cd308eb2ccdefa36ab6a239049c27b6bfc7ce531
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-10-04 20:19:42 +0100

    Restructure

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# youtube-annotation
+# YouTube Annotation
+
 Annotate YouTube videos.
+
+## Annotation format
+
+```json
+{
+  "0": "Hello this is it!",
+  "1": "Nice!",
+  "4": "Fourth second, people!"
+}
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,12 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
-var reactify = require('reactify');
+var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var streamify = require('gulp-streamify');
 
-gulp.task('js', function () {
-  return browserify('./private/js/app.js')
-        .transform(reactify)
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(gulp.dest('./public/js'));
+gulp.task('default', function () {
+  return browserify('./source/app.js')
+    .transform(babelify)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js/'));
 });
-
-gulp.task('build', function () {
-  return browserify()
-        .add('./private/js/app.js')
-        .transform(reactify)
-        .bundle()
-        .pipe(source('app.js'))
-        .pipe(streamify(uglify()))
-        .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch('./private/js/**/*.js', ['js']);
-});
-
-gulp.task('default', ['watch', 'js']);
